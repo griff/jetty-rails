ce7d38ce9508fbc01d58342aa95023b2853f963f
Brian Olsen <brian@maven-group.org>
2013-04-20 15:02:45 +0200

    Added fix for missing rdoc/usage

--- lib/jetty_rails/config/rdoc_fix.rb
+++ lib/jetty_rails/config/rdoc_fix.rb
@@ -1,28 +1,31 @@
-require 'rdoc/usage'
+begin
+  require 'rdoc/usage'
+  
+  # fix to work with rubygems (use current file instead of main)
+  def RDoc.usage_no_exit(*args)
+    comment = File.open(File.join(File.dirname(__FILE__), %w(.. .. .. bin), File.basename($0))) do |file|
+      find_comment(file)
+    end
 
-# fix to work with rubygems (use current file instead of main)
-def RDoc.usage_no_exit(*args)
-  comment = File.open(File.join(File.dirname(__FILE__), %w(.. .. .. bin), File.basename($0))) do |file|
-    find_comment(file)
+    comment = comment.gsub(/^\s*#/, '')
+
+    markup = SM::SimpleMarkup.new
+    flow_convertor = SM::ToFlow.new
+
+    flow = markup.convert(comment, flow_convertor)
+
+    format = "plain"
+
+    unless args.empty?
+      flow = extract_sections(flow, args)
+    end
+
+    options = RI::Options.instance
+    if args = ENV["RI"]
+      options.parse(args.split)
+    end
+    formatter = options.formatter.new(options, "")
+    formatter.display_flow(flow)
   end
-
-  comment = comment.gsub(/^\s*#/, '')
-
-  markup = SM::SimpleMarkup.new
-  flow_convertor = SM::ToFlow.new
-
-  flow = markup.convert(comment, flow_convertor)
-
-  format = "plain"
-
-  unless args.empty?
-    flow = extract_sections(flow, args)
-  end
-
-  options = RI::Options.instance
-  if args = ENV["RI"]
-    options.parse(args.split)
-  end
-  formatter = options.formatter.new(options, "")
-  formatter.display_flow(flow)
+rescue MissingSourceFile=>e
 end
